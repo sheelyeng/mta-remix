1225904a323f12f020ceaca9b7b0c48aad3f0324
Sheely <jamesdev0802@gmail.com>
2022-05-25 15:16:33 -0500

    WIP: old parallax

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,11 @@
 import ParallaxPage from '~/components/Parallax';
+import type { LinksFunction } from '@remix-run/node';
+
+import styles from '~/styles/parallax.css';
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 export default function Index() {
   return <ParallaxPage />;
