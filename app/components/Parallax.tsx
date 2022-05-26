d2b70739bf1866d70076830d57ad8866dc125192
Sheely <jamesdev0802@gmail.com>
2022-05-25 19:01:39 -0500

    fixed image parallax issue

--- app/components/Parallax.tsx
+++ app/components/Parallax.tsx
@@ -1,33 +1,98 @@
-import { ParallaxBanner, ParallaxProvider } from 'react-scroll-parallax';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 const ParallaxPage = () => {
   return (
-    <ParallaxProvider>
-      <div className="parallax-banner">
-        <ParallaxBanner
-          className="parallax-banner-class"
-          layers={[
-            { image: '/images/mta-landing-hero-01.png', speed: -60 },
-            { image: '/images/mta-landing-hero-02.png', speed: -55 },
-            { image: '/images/mta-landing-hero-03.png', speed: -50 },
-            { image: '/images/mta-landing-hero-04.png', speed: -45 },
-            { image: '/images/mta-landing-hero-05.png', speed: -40 },
-            { image: '/images/mta-landing-hero-06.png', speed: -35 },
-            { image: '/images/mta-landing-hero-07.png', speed: -30 },
-            { image: '/images/mta-landing-hero-08.png', speed: -25 },
-            { image: '/images/mta-landing-hero-09.png', speed: -20 },
-            { image: '/images/mta-landing-hero-10.png', speed: -15 },
-            { image: '/images/mta-landing-hero-11.png', speed: -10 },
-            {
-              image: '/images/mta-landing-hero-12.png',
-              speed: -1,
-              rootMargin: { bottom: 0, top: 100, left: 0, right: 0 },
-              shouldAlwaysCompleteAnimation: true,
-            },
-          ]}
+    <Parallax pages={1.3} style={{ top: '0', left: '0' }}>
+      <ParallaxLayer offset={0} speed={0.3} className="parallax">
+        <img
+          src="/images/mta-landing-hero-01.png"
+          alt=""
+          className="parallax-img"
         />
-      </div>
-    </ParallaxProvider>
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={0} speed={0.4} className="parallax">
+        <img
+          src="/images/mta-landing-hero-02.png"
+          alt=""
+          className="parallax-img"
+        />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={0.5} className="parallax">
+        <img
+          src="/images/mta-landing-hero-03.png"
+          alt=""
+          className="parallax-img"
+        />
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={0} speed={0.6} className="parallax">
+        <img
+          src="/images/mta-landing-hero-04.png"
+          alt=""
+          className="parallax-img"
+        />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={0.7} className="parallax">
+        <img
+          src="/images/mta-landing-hero-05.png"
+          alt=""
+          className="parallax-img"
+        />
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={0} speed={0.8} className="parallax">
+        <img
+          src="/images/mta-landing-hero-06.png"
+          alt=""
+          className="parallax-img"
+        />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={0.9} className="parallax">
+        <img
+          src="/images/mta-landing-hero-07.png"
+          alt=""
+          className="parallax-img"
+        />
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={0} speed={1} className="parallax">
+        <img
+          src="/images/mta-landing-hero-08.png"
+          alt=""
+          className="parallax-img"
+        />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={1.1} className="parallax">
+        <img
+          src="/images/mta-landing-hero-09.png"
+          alt=""
+          className="parallax-img"
+        />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={1.2} className="parallax">
+        <img
+          src="/images/mta-landing-hero-10.png"
+          alt=""
+          className="parallax-img"
+        />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={1.3} className="parallax">
+        <img
+          src="/images/mta-landing-hero-11.png"
+          alt=""
+          className="parallax-img"
+        />
+      </ParallaxLayer>
+      <ParallaxLayer offset={0} speed={1.4} className="parallax">
+        <img
+          src="/images/mta-landing-hero-12.png"
+          alt=""
+          className="parallax-img"
+        />
+        <div className="bottom" />
+      </ParallaxLayer>
+    </Parallax>
   );
 };
 
