7c45624294fe237d2fe90101e2e2dc617cf0c020
Sheely <jamesdev0802@gmail.com>
2022-05-28 00:22:47 +0500

    update some styling

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,19 @@ export const links: LinksFunction = () => {
       rel: 'stylesheet',
       href: styles,
     },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+      crossorigin: true,
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Quattrocento:wght@400;700&display=swap',
+    },
   ];
 };
 
