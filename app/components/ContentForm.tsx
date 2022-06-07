055710eb4c9f36b5cb5d9206ad2b916f33fc20c9
Sheely <jamesdev0802@gmail.com>
2022-06-07 11:05:32 -0500

    increase parallax speed

--- app/components/ContentForm.tsx
+++ app/components/ContentForm.tsx
@@ -16,62 +16,62 @@ const ParallaxPage = () => {
       <ParallaxLayer offset={0} speed={0.54}>
         <img src="/images/mta-long-page_19.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.55}>
+      <ParallaxLayer offset={0} speed={0.4}>
         <img src="/images/mta-long-page_18.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.56}>
+      <ParallaxLayer offset={0} speed={0.415}>
         <img src="/images/mta-long-page_17.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.57}>
+      <ParallaxLayer offset={0} speed={0.43}>
         <img src="/images/mta-long-page_16.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.58}>
+      <ParallaxLayer offset={0} speed={0.445}>
         <img src="/images/mta-long-page_15.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.59}>
+      <ParallaxLayer offset={0} speed={0.46}>
         <img src="/images/mta-long-page_14.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.6}>
+      <ParallaxLayer offset={0} speed={0.475}>
         <img src="/images/mta-long-page_13.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.61}>
+      <ParallaxLayer offset={0} speed={0.49}>
         <img src="/images/mta-long-page_12.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.62}>
+      <ParallaxLayer offset={0} speed={0.505}>
         <img src="/images/mta-long-page_11.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.63}>
+      <ParallaxLayer offset={0} speed={0.52}>
         <img src="/images/mta-long-page_10.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.64}>
+      <ParallaxLayer offset={0} speed={0.535}>
         <img src="/images/mta-long-page_9.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.65}>
+      <ParallaxLayer offset={0} speed={0.55}>
         <img src="/images/mta-long-page_8.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.66}>
+      <ParallaxLayer offset={0} speed={0.565}>
         <img src="/images/mta-long-page_7.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.67}>
+      <ParallaxLayer offset={0} speed={0.58}>
         <img src="/images/mta-long-page_6.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.68}>
+      <ParallaxLayer offset={0} speed={0.595}>
         <img src="/images/mta-long-page_5.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.69}>
+      <ParallaxLayer offset={0} speed={0.61}>
         <img src="/images/mta-long-page_4.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.7}>
+      <ParallaxLayer offset={0} speed={0.625}>
         <img src="/images/mta-long-page_35.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.71}>
+      <ParallaxLayer offset={0} speed={0.64}>
         <img src="/images/mta-long-page_2.svg" alt="" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.72}>
+      <ParallaxLayer offset={0} speed={0.655}>
         <img src="/images/mta-long-page_1.svg" alt="" />
         <div className="context-form-bottom" />
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.73} className="flex justify-end p-8">
+      <ParallaxLayer offset={0} speed={0.67} className="flex justify-end p-8">
         <Link to="/" className="underline text-[#ffffff]">
           Home
         </Link>
